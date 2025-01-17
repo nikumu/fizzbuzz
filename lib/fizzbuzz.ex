dc577b56e7205993c8ef2a0615df1617ccce9a6e
nikumu <andrecarlosbb@gmail.com>
2025-01-17 10:52:20 -0300

    Add pattern matching

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,8 +1,9 @@
 defmodule Fizzbuzz do
   def build(file_name) do
-    case File.read(file_name) do
-      {:ok, result} -> result
-      {:error, reason} -> reason
-    end
+    file = File.read(file_name)
+    handle_file_read(file)
   end
+
+  def handle_file_read({:ok, result}), do: result
+  def handle_file_read({:error, reason}), do: reason
 end
